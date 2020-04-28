e4aa71413b94cdecd56478e9539003e7b37a4953
Ulysse Carion <ulysse@segment.com>
2020-04-27 21:14:17 -0700

    Make public docs terser, add docs for internal stuff

--- jwt.go
+++ jwt.go
@@ -68,128 +68,52 @@ var ErrInvalidSignature = errors.New("jwt: invalid signature")
 // In order to keep the JSON representation of this struct as terse as possible,
 // all fields of this struct are omitted if left to their zero values.
 //
-// From RFC7519:
-//
-//  The following Claim Names are registered in the IANA "JSON Web Token
-//  Claims" registry established by Section 10.1.  None of the claims
-//  defined below are intended to be mandatory to use or implement in all
-//  cases, but rather they provide a starting point for a set of useful,
-//  interoperable claims.  Applications using JWTs should define which
-//  specific claims they use and when they are required or optional.  All
-//  the names are short because a core goal of JWTs is for the
-//  representation to be compact.
-//
 // StandardClaims is just a convenience struct. Do not assume that the claims in
-// StandardClaims carry any special meaning in the JWT spec.
+// StandardClaims carry any special meaning in the JWT spec. For more details on
+// the standard JWT claims, see:
+//
+// https://tools.ietf.org/html/rfc7519#section-4.1
 type StandardClaims struct {
-	// From RFC7519:
+	// Issuer identifies who issued the JWT.
 	//
-	//	The "iss" (issuer) claim identifies the principal that issued the
-	//	JWT.  The processing of this claim is generally application specific.
-	//	The "iss" value is a case-sensitive string containing a StringOrURI
-	//	value.  Use of this claim is OPTIONAL.
-	//
-	// Like all other claims in JWT, this claim does not necessarily have a
-	// special meaning across systems. You may do anything with this claim that
-	// you please, just as other systems may do.
+	// https://tools.ietf.org/html/rfc7519#section-4.1.1
 	Issuer string `json:"iss,omitempty"`
 
-	// From RFC7519:
+	// Subject identifies who the JWT is about.
 	//
-	//	The "sub" (subject) claim identifies the principal that is the
-	//	subject of the JWT.  The claims in a JWT are normally statements
-	//	about the subject.  The subject value MUST either be scoped to be
-	//	locally unique in the context of the issuer or be globally unique.
-	//	The processing of this claim is generally application specific.  The
-	//	"sub" value is a case-sensitive string containing a StringOrURI
-	//	value.  Use of this claim is OPTIONAL.
-	//
-	// Like all other claims in JWT, this claim does not necessarily have a
-	// special meaning across systems. You may do anything with this claim that
-	// you please, just as other systems may do.
+	// https://tools.ietf.org/html/rfc7519#section-4.1.2
 	Subject string `json:"sub,omitempty"`
 
-	// From RFC7519:
+	// Audience identifies who is meant to process the JWT.
 	//
-	//	The "aud" (audience) claim identifies the recipients that the JWT is
-	//	intended for.  Each principal intended to process the JWT MUST
-	//	identify itself with a value in the audience claim.  If the principal
-	//	processing the claim does not identify itself with a value in the
-	//	"aud" claim when this claim is present, then the JWT MUST be
-	//	rejected.  In the general case, the "aud" value is an array of case-
-	//	sensitive strings, each containing a StringOrURI value.  In the
-	//	special case when the JWT has one audience, the "aud" value MAY be a
-	//	single case-sensitive string containing a StringOrURI value.  The
-	//	interpretation of audience values is generally application specific.
-	//	Use of this claim is OPTIONAL.
-	//
-	// Like all other claims in JWT, this claim does not necessarily have a
-	// special meaning across systems. You may do anything with this claim that
-	// you please, just as other systems may do.
+	// https://tools.ietf.org/html/rfc7519#section-4.1.3
 	Audience string `json:"aud,omitempty"`
 
-	// From RFC7519:
+	// ExpirationTime indicates when the JWT expires. It should be a timestamp,
+	// represented as seconds since the Unix epoch.
 	//
-	//  The "exp" (expiration time) claim identifies the expiration time on
-	//  or after which the JWT MUST NOT be accepted for processing.  The
-	//  processing of the "exp" claim requires that the current date/time
-	//  MUST be before the expiration date/time listed in the "exp" claim.
-	//	Implementers MAY provide for some small leeway, usually no more than
-	//	a few minutes, to account for clock skew.  Its value MUST be a number
-	//	containing a NumericDate value.  Use of this claim is OPTIONAL.
+	// VerifyExpirationTime can help you verify whether tokens have expired.
 	//
-	// Like all other claims in JWT, this claim does not necessarily have a
-	// special meaning across systems. You may do anything with this claim that
-	// you please, just as other systems may do.
-	//
-	// See VerifyExpirationTime for a function you can use to validate this claim,
-	// if you choose to use it in the typical manner.
+	// https://tools.ietf.org/html/rfc7519#section-4.1.4
 	ExpirationTime int64 `json:"exp,omitempty"`
 
-	// From RFC7519:
+	// NotBefore indicates when the JWT becomes valid. It should be a timestamp,
+	// represented as seconds since the Unix epoch.
 	//
-	//  The "nbf" (not before) claim identifies the time before which the JWT
-	//  MUST NOT be accepted for processing.  The processing of the "nbf"
-	//  claim requires that the current date/time MUST be after or equal to
-	//  the not-before date/time listed in the "nbf" claim.  Implementers MAY
-	//  provide for some small leeway, usually no more than a few minutes, to
-	//  account for clock skew.  Its value MUST be a number containing a
-	//  NumericDate value.  Use of this claim is OPTIONAL.
+	// VerifyNotBefore can help you verify whether a token is valid yet.
 	//
-	// Like all other claims in JWT, this claim does not necessarily have a
-	// special meaning across systems. You may do anything with this claim that
-	// you please, just as other systems may do.
-	//
-	// See VerifyNotBefore for a function you can use to validate this claim, if
-	// you choose to use it in the typical manner.
+	// https://tools.ietf.org/html/rfc7519#section-4.1.5
 	NotBefore int64 `json:"nbf,omitempty"`
 
-	// From RFC7519:
+	// IssuedAt indicates when the JWT was issued. It should be a timestamp,
+	// represented as seconds since the Unix epoch.
 	//
-	//	The "iat" (issued at) claim identifies the time at which the JWT was
-	//	issued.  This claim can be used to determine the age of the JWT.  Its
-	//	value MUST be a number containing a NumericDate value.  Use of this
-	//	claim is OPTIONAL.
-	//
-	// Like all other claims in JWT, this claim does not necessarily have a
-	// special meaning across systems. You may do anything with this claim that
-	// you please, just as other systems may do.
+	// https://tools.ietf.org/html/rfc7519#section-4.1.6
 	IssuedAt int64 `json:"iat,omitempty"`
 
-	// From RFC7519:
+	// ID is a unique identifier for the JWT.
 	//
-	//	The "jti" (JWT ID) claim provides a unique identifier for the JWT.
-	//	The identifier value MUST be assigned in a manner that ensures that
-	//	there is a negligible probability that the same value will be
-	//	accidentally assigned to a different data object; if the application
-	//	uses multiple issuers, collisions MUST be prevented among values
-	//	produced by different issuers as well.  The "jti" claim can be used
-	//	to prevent the JWT from being replayed.  The "jti" value is a case-
-	//	sensitive string.  Use of this claim is OPTIONAL.
-	//
-	// Like all other claims in JWT, this claim does not necessarily have a
-	// special meaning across systems. You may do anything with this claim that
-	// you please, just as other systems may do.
+	// https://tools.ietf.org/html/rfc7519#section-4.1.7
 	ID string `json:"jti,omitempty"`
 }
 
@@ -220,20 +144,6 @@ var ErrExpiredToken = errors.New("jwt: expired token")
 // make sure you populate the ExpirationTime ("exp") field in StandardClaims by
 // calling the Unix function on a time.Time instance. If you use UnixNano
 // instead of Unix, VerifyExpirationTime will return invalid results.
-//
-// From RFC7519:
-//
-//  The "exp" (expiration time) claim identifies the expiration time on
-//  or after which the JWT MUST NOT be accepted for processing.  The
-//  processing of the "exp" claim requires that the current date/time
-//  MUST be before the expiration date/time listed in the "exp" claim.
-//  Implementers MAY provide for some small leeway, usually no more than
-//  a few minutes, to account for clock skew.  Its value MUST be a number
-//  containing a NumericDate value.  Use of this claim is OPTIONAL.
-//
-// This package does not implement the "small leeway" described in the spec. If
-// you want to tolerate a slightly-expired token, you should adjust "now" to be
-// a bit after time.Now.
 func (s *StandardClaims) VerifyExpirationTime(now time.Time) error {
 	if now.After(time.Unix(s.ExpirationTime, 0)) {
 		return ErrExpiredToken
@@ -258,20 +168,6 @@ func (s *StandardClaims) VerifyExpirationTime(now time.Time) error {
 // sure you populate the NotBefore ("nbf") field in StandardClaims by calling
 // the Unix function on a time.Time instance. If you use UnixNano instead of
 // Unix, VerifyNotBefore will return invalid results.
-//
-// From RFC7519:
-//
-//  The "nbf" (not before) claim identifies the time before which the JWT
-//  MUST NOT be accepted for processing.  The processing of the "nbf"
-//  claim requires that the current date/time MUST be after or equal to
-//  the not-before date/time listed in the "nbf" claim.  Implementers MAY
-//  provide for some small leeway, usually no more than a few minutes, to
-//  account for clock skew.  Its value MUST be a number containing a
-//  NumericDate value.  Use of this claim is OPTIONAL.
-//
-// This package does not implement the "small leeway" described in the spec. If
-// you want to tolerate a soon-to-be-valid token, you should adjust "now" to be
-// a bit before time.Now.
 func (s *StandardClaims) VerifyNotBefore(now time.Time) error {
 	if now.Before(time.Unix(s.NotBefore, 0)) {
 		return ErrExpiredToken
